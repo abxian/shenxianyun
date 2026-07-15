d117f4195a70ae118f2832a0a38cc81f9a8fbbe3
abxian <abxian@users.noreply.github.com>
2026-07-15 23:30:29 +0800

    fix: 补全更新清单版本字段

--- scripts/updater.mjs
+++ scripts/updater.mjs
@@ -86,6 +86,7 @@ async function processRelease(github, options, tag, isAlpha) {
 
     const updateData = {
       name: tag.name,
+      version: tag.name.replace(/^v/, ''),
       notes: await resolveUpdateLog(tag.name).catch(() =>
         resolveUpdateLogDefault().catch(() => 'No changelog available'),
       ),
